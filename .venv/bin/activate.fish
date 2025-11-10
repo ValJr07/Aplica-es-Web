0a9ae47caed75f1fde12fc41cf58db8c3c7aa3ad
Valter de Sales <vsalesjr2008@gmail.com>
2025-11-10 15:07:45 -0300

    SQLite update

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/estudante2/Área de trabalho/Aplica-es-Web/.venv'
+set -gx VIRTUAL_ENV '/home/estudante1/Área de trabalho/Aplica-es-Web/.venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
--- outubro/20.10.2025/app.py
+++ outubro/20.10.2025/app.py
@@ -1,8 +1,52 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
-#BD 
+connector = sqlite3.connect('Hospedagem.db')
+with connector as connection:
+    cursor = connector.cursor()
+
+    create_table_quarto = '''
+    CREATE TABLE Quarto(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
+        preco FLOAT,
+        descricao TEXT,
+        imagem TEXT
+    );    
+    '''
+
+    insert_quarto = '''
+    INSERT INTO Quarto(nome, preco, descricao, imagem)
+    VALUES(?, ?, ?, ?);
+    '''
+
+    delete_quarto = '''
+    DELETE FROM Quarto
+    WHERE id = ?;
+    '''
+
+    quarto_id = 1
+
+
+    data_quarto1 = ("Suíte Luxo Vista Mar", 550.00, "Ampla, com varanda e vista panorâmica para o mar.", "https://picsum.photos/seed/quarto1/600/400")
+    data_quarto2 = ("Suíte Standard", 320.00, "Aconchegante e moderna, ideal para casais.", "https://picsum.photos/seed/quarto2/600/400")
+    data_quarto3 = ("Quarto Econômico", 210.00, "Simplicidade e conforto pelo melhor preço.", "https://picsum.photos/seed/quarto3/600/400")
+
+    select_quarto = "SELECT * FROM Quarto;"
+
+    cursor.execute(select_quarto)
+
+    todos_quartos = cursor.fetchall()
+
+
+    print("todos os quartos:")
+    for quarto in todos_quartos:
+        print(quarto)
+
+#BD
+'''
 quartos = [
     {
         "id": 1,
@@ -26,14 +70,15 @@ quartos = [
         "imagem": "https://picsum.photos/seed/quarto3/600/400"
     }
 ]
+'''
 
 @app.route('/')
 def index():
-    return render_template('index.html', quartos=quartos)
+    return render_template('index.html', quarto=todos_quartos)
 
 @app.route('/quarto/<int:id>')
 def quarto(id):
-    quarto_escolhido = next((q for q in quartos if q["id"] == id), None)
+    quarto_escolhido = next((q for q in todos_quartos if q["id"] == id), None)
     if quarto_escolhido:
         return render_template('quarto.html', quarto=quarto_escolhido)
     return "<h2>Quarto não encontrado 😢</h2>", 404
